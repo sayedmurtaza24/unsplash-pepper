0e9f24f728b1d679181b3dcbec16a427ca76f352
Mori <sayedmurtazamuttahary@gmail.com>
2022-06-17 17:17:37 +0200

    fixed response json

--- src/unsplash.js
+++ src/unsplash.js
@@ -1,5 +1,5 @@
 function getData(searchTerm, callback) {
-  fetch(`https://unsplash-pepper.herokuapp.com/api/get-unsplash-photo/?searchTerm=${searchTerm}`).then(callback);
+  fetch(`https://unsplash-pepper.herokuapp.com/api/get-unsplash-photo/?searchTerm=${searchTerm}`).then(data => data.json()).then(callback);
 }
 
 export default getData;
