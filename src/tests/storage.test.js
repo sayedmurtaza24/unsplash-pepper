50c4342ee4220ee6ec26830c25af696c74b71dc5
Mori <sayedmurtazamuttahary@gmail.com>
2022-06-17 10:44:19 +0200

    finished

---
+++ src/tests/storage.test.js
@@ -0,0 +1,27 @@
+import { getSearchTerms, saveSearchTerm } from '../storage';
+
+beforeAll(() => {
+   class LocalStorageMock {
+      constructor() { this.store = {}; }
+      clear() { this.store = {}; }
+      getItem(key) { return this.store[key] || null; }
+      setItem(key, value) { this.store[key] = String(value); }
+      removeItem(key) { delete this.store[key]; }
+   }
+   global.localStorage = new LocalStorageMock();
+})
+
+afterEach(() => {
+   global.localStorage.clear();
+})
+
+test('saves searched term to the localStorage', () => {
+   saveSearchTerm('apple');
+   expect(global.localStorage.store).toEqual({ recentSearches: "[\"apple\"]" });
+})
+
+test('loads back all the searched terms into an array', () => {
+   saveSearchTerm('apple');
+   saveSearchTerm('pirate');
+   expect(getSearchTerms()).toEqual(['apple', 'pirate']);
+})
---
+++ src/tests/unsplash.spec.js
@@ -0,0 +1,5 @@
+import { getData } from '../unsplash.js';
+
+  test('getData function should output 10 results on our page', () => {
+    expect(output()).toEqual();
+  });
